5e1185becb04ca5d0885a4c78c5a0b77fad6adef
Zsolt Bangha <Zsolt_Bangha@epam.com>
2019-06-26 12:02:37 +0200

    Batch size limitation

--- Yutbube/EnqueuerFunction.cs
+++ Yutbube/EnqueuerFunction.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
